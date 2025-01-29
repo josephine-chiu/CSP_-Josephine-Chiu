da11b86eaedc8fb6c13f51c540b26b10d12c3460
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-01-29 09:52:43 -0700

    python practice

--- variables/notes.py
+++ variables/notes.py
@@ -5,3 +5,7 @@ name = input("Please tell me your name: ")
 print(name+ ", welcome to my program.")
 
 print("It doesn't do anything yet")
+
+name = "alex"
+
+name = "tia"
---
+++ variables/practice.py
@@ -0,0 +1,9 @@
+# Josephine Chiu, Variables Practice Python
+
+name = print("Josephine")
+
+smallnumber = print("five")
+
+bignumber = print("two hundred")
+
+breakfast = 
