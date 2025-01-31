74838c82006bf905c53cfc4e5dc16e97299fe061
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-01-31 09:34:38 -0700

    finished practice c

---
+++ practice.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+char name[]= "Josephine";
+int small_num = 6;
+int num = 254;
+char breakfast[] = "toast";
+char color[] = "pink";
+char school[] = "UCAS";
+char year[] = "2025";
+char eye_color[] = "brown";
+int age = 14;
+char subject [] = "math";
+
+int main(void){
+    printf("%s\n", name);
+
+    printf("%d\n", small_num);
+
+    printf("%d\n", num);
+
+    printf("%s\n", breakfast);
+
+    printf("%s\n", color);
+
+    printf("%s\n", school);
+
+    printf("%s\n", year);
+
+    printf("%s\n", eye_color);
+
+    printf("%d\n", age);
+
+    printf("%s\n", subject);
+
+
+
+
+
+
+
+    return 0;
+}
