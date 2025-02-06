c7932dfcdc080a3e92d8e2e9fe7bcc4f3244e763
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-06 09:48:34 -0700

    added notes to financial calc.py

--- expressions/financial_calculator.py
+++ expressions/financial_calculator.py
@@ -1,1 +1,35 @@
 # Josephine Chiu, Financial Calculator for Python
+
+# write a print statemnet telling the user what the program is (budget calculator)
+
+# ask for monthly income (user input)
+
+# ask for rent amount (user input)
+# ask for utilities (water, electricity) (user input)
+# ask for groceries (user input)
+# ask for transportation (car payment, bus pass) (user input)
+
+# Calculate savings as 10% of income (variable)
+# Calculate spending money income - (rent + utilities + groceries + transportation) -> variable
+
+# Calculate percent of rent (rent/income) variable 
+# Calculate percent of utilities (utilities/income) variable 
+# Calculate percent of groceries (groceries/income) variable 
+# Calculate percent of transportation (transportation/income) variable
+# Calculate percent of spending (spending/income) variable
+
+# tell user category spending amount AND percent for rent ("You spend $XX.XX on rent and that is XX% of your income")
+
+# tell user category spending amount AND percent for utilities ("You spend $XX.XX on utilities and that is XX% of your income")
+
+# tell user category spending amount AND percent for groceries ("You spend $XX.XX on groceries and that is XX% of your income")
+
+# tell user category spending amount AND percent for transportation ("You spend $XX.XX on transportation and that is XX% of your income")
+
+# tell user category spending amount AND percent for spending ("You spend $XX.XX on spending and that is XX% of your income")
+
+# tell user category savings amount AND percent for savings ("You spend $XX.XX on rent and that is 10% of your income")
+
+income = float(input("What is your monthly income\n"))
+
+print("Hello! This is your personal budget calculator! ")
