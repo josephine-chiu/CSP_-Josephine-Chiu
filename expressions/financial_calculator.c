b6097d87e90913b232af5996239f3c4a7660f938
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-07 09:53:24 -0700

    started on financial calc. for c :/

--- expressions/financial_calculator.c
+++ expressions/financial_calculator.c
@@ -1,7 +1,35 @@
 // Josephine Chiu, Financial Calculator for C
 #include <stdio.h>
 
+char welcome[44] = "Hello! This is your personal financial calculator!";
+
+// ask for monthly income (user input)
+float = income;
+
+// ask for rent amount (user input)
+float = rent = float(input
+
+// ask for utilities (water, electricity) (user input)
+utilities = float(input("How much do your monthly utilities cost?\n"));
+
+// ask for groceries (user input)
+groceries = float(input("How much do your monthly groceries cost?\n"));
+
+// ask for transportation (car payment, bus pass) (user input)
+transportation = float(input("How much is your monthly transportation?\n"));
+
+
+
+
 int main(void){
     // introduce user to program
+printf("What is your monthly income?\n"));
+
+
+printf("How much does your rent cost?\n"));
+
+
+
+    
     return 0;
 }
