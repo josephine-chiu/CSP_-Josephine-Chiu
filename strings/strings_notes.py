323db25e8b4e0e42aa15a0d73974d505dfb93873
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-10 09:53:52 -0700

    started strings notes for python

---
+++ strings/strings_notes.py
@@ -0,0 +1,23 @@
+# Josephine Chiu, Strings Notes for Python
+
+# a string is words in programming (data type)"" ''
+
+#name = input("what is your name\n").strip().lower()
+
+#name = input("what is your name\n").strip().upper()
+
+name = input("what is your name\n").strip().capitalize()
+
+print(f"Hello {name} welcome to my program\n")
+
+name = input("Please give me a number\n") #.stirp().capitalize()
+num = 5*2+name
+print(num)
+print(name + 2)
+
+#to concatenate strings, you put a plus sign to add them together.
+
+sentence = "the quick brown fox jumps over the lazy dog"
+
+print(sentence.find("fox"))
+print(sentence.split)
