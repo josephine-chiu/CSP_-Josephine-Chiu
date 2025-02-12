4d9c84a97a059c87e252c86fa936b6fea098aa92
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-12 23:17:31 +0000

    added onto name decorator for c

--- strings/strings_name_decorator.c
+++ strings/strings_name_decorator.c
@@ -1,24 +1,20 @@
 // Josephine Chiu, Name Decorator for C
 #include <stdio.h>
-#include <stdio.h>
+#include <string.h>
 
 char name[20];
-
+char dec_one[] = "~~~";
+char dec_two[] = "~~~";
 
 int main(void){
     printf("Hello! Please tell me your nickname:\n");
-    scanf("%s", name);
+    scanf("%s", &name);
     printf("That is a wonderful name, %s! Welcome to my program. I'm glad you're here!\n", name);
 
-char dec_1[] = "~~~";
-char dec_2[] = "~~~";
-
-    printf("Here is your decoratd name!", dec_1, name, dec_2);
-    strcat(dec_1, name);
+    strcat(dec_one, name);
+    strcat(dec_one, dec_two);
+    printf("This is your decorated name!%s", dec_one);
     return 0;
-}
-
-
 
 //decoration_1 = ("~~~")
 //decoration_2 = ("~~~")
