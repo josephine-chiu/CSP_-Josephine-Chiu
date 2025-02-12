5363610f7dfc5c2b4a49feaee0419b8cddf8d739
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-12 09:26:15 -0700

    finished name decorator for python!!!

---
+++ strings/strings_name_decorator.c
@@ -0,0 +1,7 @@
+// Josephine Chiu, Name Decorator for C
+#include <stdio.h>
+
+int main(void){
+    printf("Hello world");
+    return 0;
+}
---
+++ strings/strings_name_decorator.py
@@ -0,0 +1,8 @@
+# Josephine Chiu, Name Decorator for Python
+
+name = input("What is your name?\n")
+print(f"That is a lovely name, {name}!")
+decoration_1 = ("~~~")
+decoration_2 = ("~~~")
+print(decoration_1+name+decoration_2)
+print("I hope you like your decorated name!")
