f7f918673d0d317ef45e1dda7e210a8e5bcbb0c2
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-12 23:34:14 +0000

    finished name decorator for c!!!

--- strings/strings_notes.c
+++ strings/strings_notes.c
@@ -5,25 +5,14 @@
 char name[20];
 
 int main(void){
-    printf("Please tell me your full name:\n");
-    //scanf("%s", name);
-    //fgets(name, 20, stdin); allows full name to be printed
-    //printf("Hello %s, welcome to my program", name);
+    printf("Hello! Please tell me your nickname:\n");
+    scanf("%s", &name);
+    printf("That is a wonderful name, %s! Welcome to my program. I'm glad you're here!\n", name);
+char dec_one[] = "~~~";
+char dec_two[] = "~~~";
 
-    char sentence[] = "The quick brown fox jumps over the lazy dog";
-    //printf("%s\n", sentence[16:18]);
-
-    //printf("%lu\n", sizeof(sentence)); //sizeof will always give me one more than strlen
-    //printf("%d\n", strlen(sentence)); // 43 PHYSICAL characters
-
-    char one[] = "Hello";
-    char two[]= " World!";
-    char three[] = "This is my program. ";
-    two[6] =  '?';
-
-    printf("%s\n", one);
-    printf("%s\n", two);
-    strcat(one,two); // put the variables in the order you want them to be
-    strcat(three, one);
+    strcat(dec_one, name);
+    strcat(dec_one, dec_two);
+    printf("This is your decorated name!\n%s", dec_one);
     return 0;
 }
