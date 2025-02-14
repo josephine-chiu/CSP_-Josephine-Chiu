485090d3ce13bab6e3350e3d69a23087eda79cf8
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-14 09:41:57 -0700

    finished silly sentences for python

--- strings/silly_sentences.py
+++ strings/silly_sentences.py
@@ -1,17 +1,11 @@
 # Josephine Chiu, Silly Sentences for Python
 
+#introdue user to program...tell them whatt is happening (print statement)
+print("Hello! Welcome to your personal Silly Sentences maker! Please answer the following questions:\n")
+
 #string variables for my user inputs (minimum of 3)
 animal = input("What is your favorite animal?\n")
+celebrity = input("Who is your favorite female celebrity?\n")
+food = input("What is your least favorite food?\n")
 
-
-
-
-#introdue user to program...tell them whatt is happening (print statement)
-
-print("Hello! Welcome to your personal Silly Sentences maker! Please give me a random adjective, verb, and noun:\n");
-
-
-
-#create user inputs (print statments with questions AND scanf to collect the info)(same as the number of variables)
-#insert variables into the sentence to show the user (print statement, only 1!!) example:"hello %s", name
-#\n=new line
+print("Behold, your silly sentence! I was riding home from school when I saw", celebrity, "scrambling to escape from a herd of", animal, "with a bundle of", food, "flying from her hands. Celebrities; they're always running away from somehting.\n")
