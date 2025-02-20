25fbbe076e33b073d332d3d3b4606701679561b6
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-20 19:43:36 +0000

    vincent helped me with update hello world. thank you vincent :)

--- functions/update_Hello_World.py
+++ functions/update_Hello_World.py
@@ -1,9 +1,15 @@
 # Josephine Chiu, Update Hello World for Python
 
 def user(name):
-    return(f"Hello {name}\n")
-name1 = user("Dave")
-name2 = user("John")
-name3 = user("Bob")
-name4 = user("Mary")
-name5 = user("Sasha")
+    return "Hello " + name + "!"
+print(user("Dave"))
+print(user("John"))
+print(user("Bob"))
+print(user("Vincent"))
+print(user("Sasha"))
+
+#def main(name):
+#   return "Hello " + name + "!"
+#for i in range(5):
+#   print(main(input("What is the name?\n")))
+#This was me, Vincent, I wanted to show how I could do it in 4 lines, and Josephine needed help...so...
