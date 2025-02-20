a7fd655a44f3cc1414b7f31763d747652fd355aa
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-20 16:03:46 -0700

    jasper helped with update hello world for c

--- functions/update_Hello_World.c
+++ functions/update_Hello_World.c
@@ -1,12 +1,15 @@
 // Josephine Chiu, Update Hello World for C
 #include <stdio.h>
 
-char add(char type[20]){
-    char name[20];
-    printf("%s\n", name);
+char user(char name[20]){
+    printf("Hello %s",name); //the variable "name" is the parameter above
 }
 
 int main(void){
-    printf("Hello world");
+    user("Jasper\n"); //using user(), you can put a name in the parentheses and it will be put into the greeting phrase
+    user("Amon\n");
+    user("Sam\n");
+    user("Vincent\n");
+    user("Sasha\n");
     return 0;
 }
