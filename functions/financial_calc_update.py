c0b14b3de775de017c8414a0e72f6b842e900501
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-22 04:33:52 +0000

    finished fin. calc update for python

--- functions/financial_calc_update.py
+++ functions/financial_calc_update.py
@@ -1,15 +1,12 @@
 # Josephine Chiu, Financial Calculator Update for Python
 
+# Josephine Chiu, Financial Calculator Update for Python
 print("Hello! This is your personal budget calculator! Please follow the instructions to learn more about your monthly finances.")
+
 def user(type):
     question = "How much is your monthly " + type + "?\n"
     return float(input(question))
 
-def percent(income, amount, type):
-    percent_type = float(amount/income*100)
-    print(f"You spent ${amount:.2f} on {type} and that is {percent_type:.0f}% of your income.")
-    
-    print(f"Your total spending amount is {spending} and that is {percent_type:.0f}% of your income.")
 income = user("income")
 rent = user("rent")
 utilities = user("utilities")
@@ -19,9 +16,15 @@ transportation = user("transportation")
 savings = income*0.1
 spending = rent+utilities+groceries+transportation
 
+def percent(income, amount, type):
+    percent_type = float(amount/income*100)
+    print(f"You spent ${amount:.2f} on {type} and that is {percent_type:.0f}% of your income")
+
 percent(income, rent, "rent")
 percent(income, utilities, "utilities")
 percent(income, groceries, "groceries")
 percent(income, transportation, "transportation")
-percent(income, savings, "savings")
 percent(income, spending, "spending")
+
+print("We are at line 27")
+print(f"You saved ${savings:.2f} and that is 10% of your income")
