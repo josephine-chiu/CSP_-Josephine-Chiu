8f20dacddca03fed125704a7fe4dcff5275bcfa3
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-26 22:40:21 +0000

    started on old enough for c

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void){
-    printf("Hello world");
+int age;
+
+int main(void) {
+int age = 16;
+printf("How old are you?\n");
+scanf("%d", &age);
+if(age, "16"){
+    printf("You're old enough to drive!\n");
+    printf("We are at line 13");
     return 0;
-}
+ }
