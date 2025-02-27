f3bd59e44186bb1af0df675537c85abeee82afbb
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-27 16:17:09 -0700

    corrected old enough c :)

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -10,6 +10,8 @@ elif age == 14:
     print("Unfortunately, you can't vote,you can't drive, and you can't get a learners permit. HOWEVER, you can go to school though, so you know, yay.\n")
 elif age == 15:
     print("You're special!! You can't vote, but you can drive if you get a learners permit which you're old enough to get. Also, you can go to school so you know, yay.\n")
+elif age == 16:
+    print("You can't vote, but you can drive if you get a learners permit which you're old enough to get. Also, you can go to school so you know, yay.\n")
 elif age >=18:
     print("You are not old enough to vote, BUT you are old enough to drive, you are old enough to get a learners permit, and you are old enough to go to school.\n")
 else:
