239264a511343b7f3cccf94e7f3bf1b4cde02dae
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-02-27 19:43:52 +0000

    vincent helped me with old enough python :)

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -1,12 +1,13 @@
 # Josephine Chiu, Old Enough Practice for Python
 print("Hello! Welcome to my program that will sort you into a category based on your age!\n")
 age = int(input("How old are you?\n")) 
-
-if age ==16:
-    print("You are not old enough to vote, BUT you are old enough to drive, you are old enough to get a learners permit, and you are old enough to go to school.\n")
-elif age <=14:
+if age < 14:
+    print()
+elif age == 14:
     print("Unfortunately, you can't vote,you can't drive, and you can't get a learners permit. HOWEVER, you can go to school though, so you know, yay.\n")
 elif age ==15:
-    print("You're special!! You can't vote, but you can drive if you get a learners permit which you're old enough to get. Also, you can go to school so you know, yay.\n")
+    print("You're special!! You can't vote, but you can drive if you get a learners permit which you're old enough to get. Also, you can go to school so you know, yay.\n"
+elif age ==16:
+    print("You are not old enough to vote, BUT you are old enough to drive, you are old enough to get a learners permit, and you are old enough to go to school.\n")
 else:
     print("You are FINALLY old enough to vote, you are old enough to drive, you are old enough to get a learners permit, and you are old enough to go to school! Hooray!\n")
