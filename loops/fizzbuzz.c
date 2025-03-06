a3f1ff79143b65b4faaab13cdceaf019f10f4f80
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-03-06 16:06:20 -0700

    sasha and adelle helped me with fizzbuzz for c

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -1,7 +1,17 @@
 // Josephine Chiu, FizzBuzz for C
 #include <stdio.h>
 
+int num;
 int main(void){
-    printf("Hello world");
+    for(num=0; num<51; num++)
+        if(num % 3 == 0 && num % 5 == 0){
+            printf("FizzBuzz\n");
+        }else if(num % 5 == 0){
+            printf("Buzz\n");
+        }else if (num % 3 == 0){
+            printf("Fizz\n");
+        }else{
+            printf("%d\n", num);
+        }
     return 0;
 }
