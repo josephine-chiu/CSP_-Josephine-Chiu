8027b7b0b1d8c6ccb0f9855e2ee7cb4110256cec
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-03-06 15:51:13 -0700

    sasha helped me with fizz buzz for python

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,15 +1,10 @@
 # Josephine Chiu, FizzBuzz for Python
-
-num = 0
-while num >= 0 and num < 51:
-nums = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48]
-for nums in nums:
-    print("Fizz")
-
-while num <= 50:
-    if num !=
-        print(f"Fizz")
-        break #tells the loop to be done
+for num in range(1,51):
+    if num % 3 == 0 and num % 5 == 0: 
+        print("Fizzbuzz")
+    elif num % 5 == 0:
+        print("Buzz")
+    elif num % 3 == 0:
+        print("Fizz")
     else:
-        print("Duck")
-    num += 1
+        print(num)
