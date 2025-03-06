d2091768c60d689c8c34f286cb04acc0cf0e1883
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-03-06 09:18:30 -0700

    finished my family loop for python

---
+++ loops/my_fam_loop.c
@@ -0,0 +1,7 @@
+// Josephine Chiu, My Family Loop for C
+#include <stdio.h>
+
+int main(void){
+    printf("Hello world");
+    return 0;
+}
---
+++ loops/my_fam_loop.py
@@ -0,0 +1,4 @@
+# Josephine Chiu, My Family Loop for Python
+families = ["Mom", "Ben", "Elle", "Emma"]
+for family in families:
+    print(f"Hi {family}! I'm so happy to see you!")
