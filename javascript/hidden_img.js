00f070cdb567e611c88cfb40ac4826b56198e184
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-04-24 16:31:55 -0600

    confused on hidden image still

--- javascript/hidden_img.js
+++ javascript/hidden_img.js
@@ -1,14 +1,3 @@
-document = hidden_img.html
-count = 0
 function change(){
-    document.getElementById("img").src = images[count]
-    if(count === 2){
-        count = 0
-    }else{
-        count += 1
-    }
-}
-
-function swich(){
-    document.getElementById("hidden tiger").style.display = "block"
+    document.getElementById("hidden").style.display = "block"
 }
