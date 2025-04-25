d848cf9a23d4c0a0a1efac6bd8b3e410e11f7d96
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-04-25 16:32:39 -0600

    uhhhh didn't do anything for my website....

--- first_html_webpage/first_webpage.js
+++ first_html_webpage/first_webpage.js
@@ -1,10 +1,12 @@
 document = first_webpage.html
-function more(){
-    if(document.getElementById("extra"). style.display != "flex"){
-    document.getElementById("extra").style.display = "flex"
-document.getElementById("show").innerHTML = "Show less"
+
+ function more(){
+    if(document.getElementById("btn").style.display != "flex"){
+        document.getElementById("btn").style.display = "flex"
+        document.getElementById("show").innerHTML = "Show less"
     }else{
-        document.getElementById ("extra").style.display = "none"
-    document.getElementById ("show").innerHTML = "Show more"
+        document.getElementById("btn").style.display = "none"
+        document.getElementById("show").innerHTML = "Show more"
     }
-}
+ }
+
