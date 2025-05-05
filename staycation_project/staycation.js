5e010282632ad49a2dabf6d77596aa66fb593472
josephine-chiu <josephine.chiu@ucas-edu.net>
2025-05-05 16:31:46 -0600

    fix the formatting of the page. need to work on button and hoverable image. you got this

--- staycation_project/staycation.js
+++ staycation_project/staycation.js
@@ -9,3 +9,9 @@ function more() {
         btn.textContent = "Show More";
     }
 }
+
+<div id="img2">
+    <img src="https://images.ctfassets.net/0wjmk6wgfops/3WRVZrwr311SwlyzfTydlt/df15f5b0ab7a985f0762196bed3ee1f0/Antelope_Island_blue_skies_stock_AdobeStock_313773770.jpeg?w=1200&h=630&f=center&fit=fill"
+    alt="Pretty mountains">
+    <p id="link2">Image from: https://www.utah.com/destinations/state-parks/antelope-island-state-park/</p>
+</div>
